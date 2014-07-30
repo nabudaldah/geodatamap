4fa9b5f87ea39ee805f5aa73d0ff4f7b2e665669
nabudaldah <nabi.abudaldah@gmail.com>
2014-07-30 22:54:15 +0200

    Initial online version
    Developed locally

---
+++ data/meters.js
@@ -0,0 +1,562 @@
+var metadata = {
+	ALLOC_301114 : {
+		id : "ALLOC_301114",
+		name : "Grijpskerk UGS (NAM)",
+		tags : ["berging"],
+		place : "GRIJPSKERK",
+		lat : 53.270000,
+		lon : 6.300000
+	},
+	ALLOC_301116 : {
+		id : "ALLOC_301116",
+		name : "Norg UGS (NAM)",
+		tags : ["berging"],
+		place : "NORG",
+		lat : 53.070000,
+		lon : 6.470000
+	},
+	ALLOC_301118 : {
+		id : "ALLOC_301118",
+		name : "Alkmaar UGS (TAQA)",
+		tags : ["berging"],
+		place : "ALKMAAR",
+		lat : 52.630000,
+		lon : 4.750000
+	},
+	ALLOC_301149 : {
+		id : "ALLOC_301149",
+		name : "Grijpskerk (NAM Procesgas)",
+		tags : ["berging"],
+		place : "GRIJPSKERK",
+		lat : 53.270000,
+		lon : 6.300000
+	},
+	ALLOC_301150 : {
+		id : "ALLOC_301150",
+		name : "Norg (NAM Procesgas)",
+		tags : ["berging"],
+		place : "NORG",
+		lat : 53.070000,
+		lon : 6.470000
+	},
+	ALLOC_301198 : {
+		id : "ALLOC_301198",
+		name : "Enschede (Epe UGS Essent)",
+		tags : ["berging"],
+		place : "ENSCHEDE",
+		lat : 52.220000,
+		lon : 6.900000
+	},
+	ALLOC_301309 : {
+		id : "ALLOC_301309",
+		name : "Enschede (Nuon UGS Epe)",
+		tags : ["berging"],
+		place : "ENSCHEDE",
+		lat : 52.220000,
+		lon : 6.900000
+	},
+	ALLOC_301320 : {
+		id : "ALLOC_301320",
+		name : "Zuidwending (Nuon-UGS)",
+		tags : ["berging"],
+		place : "ZUIDWENDING",
+		lat : 53.100000,
+		lon : 6.930000
+	},
+	ALLOC_953001 : {
+		id : "ALLOC_953001",
+		name : "Bbl Julianadorp",
+		tags : ["berging"],
+		place : "JULIANADORP",
+		lat : 52.900000,
+		lon : 4.750000
+	},
+	ALLOC_953002 : {
+		id : "ALLOC_953002",
+		name : "Bbl Bacton (M3 Normal)",
+		tags : ["berging"],
+		place : "BACTON",
+		lat : 52.850370,
+		lon : 1.477450
+	},
+	ALLOC_953003 : {
+		id : "ALLOC_953003",
+		name : "Bbl Bacton (M3 Standard)",
+		tags : ["berging"],
+		place : "BACTON",
+		lat : 52.850370,
+		lon : 1.477450
+	},
+	ALLOC_301069 : {
+		id : "ALLOC_301069",
+		name : "Anjum (NAM)",
+		tags : ["entry"],
+		place : "ANJUM",
+		lat : 53.370000,
+		lon : 6.150000
+	},
+	ALLOC_301070 : {
+		id : "ALLOC_301070",
+		name : "Annerveen (NAM)",
+		tags : ["entry"],
+		place : "ANNERVEEN",
+		lat : 53.070000,
+		lon : 6.780000
+	},
+	ALLOC_301071 : {
+		id : "ALLOC_301071",
+		name : "Balgzand Hc (NAM)",
+		tags : ["entry"],
+		place : "BALGZAND",
+		lat : 52.900000,
+		lon : 4.850000
+	},
+	ALLOC_301072 : {
+		id : "ALLOC_301072",
+		name : "Balgzand Lc (NAM)",
+		tags : ["entry"],
+		place : "BALGZAND",
+		lat : 52.900000,
+		lon : 4.850000
+	},
+	ALLOC_301073 : {
+		id : "ALLOC_301073",
+		name : "Balgzand Nogat (NAM)",
+		tags : ["entry"],
+		place : "BALGZAND",
+		lat : 52.900000,
+		lon : 4.850000
+	},
+	ALLOC_301074 : {
+		id : "ALLOC_301074",
+		name : "Barendrecht (NAM)",
+		tags : ["entry"],
+		place : "BARENDRECHT",
+		lat : 51.850000,
+		lon : 4.550000
+	},
+	ALLOC_301075 : {
+		id : "ALLOC_301075",
+		name : "Bedum (NAM)",
+		tags : ["entry"],
+		place : "BEDUM",
+		lat : 53.300000,
+		lon : 6.600000
+	},
+	ALLOC_301077 : {
+		id : "ALLOC_301077",
+		name : "Beverwijk (Clyde V142)",
+		tags : ["entry"],
+		place : "BEVERWIJK",
+		lat : 52.480000,
+		lon : 4.650000
+	},
+	ALLOC_301076 : {
+		id : "ALLOC_301076",
+		name : "Blija (NAM)",
+		tags : ["entry"],
+		place : "BLIJA",
+		lat : 53.000000,
+		lon : 6.000000
+	},
+	ALLOC_301078 : {
+		id : "ALLOC_301078",
+		name : "Botlek (NAM)",
+		tags : ["entry"],
+		place : "BOTLEK",
+		lat : 51.880000,
+		lon : 4.290000
+	},
+	ALLOC_301375 : {
+		id : "ALLOC_301375",
+		name : "Brakel Wijk Aalburg (Northern Petroleum)",
+		tags : ["entry"],
+		place : "BRAKEL",
+		lat : 51.820000,
+		lon : 5.100000
+	},
+	ALLOC_301113 : {
+		id : "ALLOC_301113",
+		name : "Emden Ept (GASSCO)",
+		tags : ["entry"],
+		place : "EMDEN",
+		lat : 53.360000,
+		lon : 7.210000
+	},
+	ALLOC_301112 : {
+		id : "ALLOC_301112",
+		name : "Emden Npt (GASSCO)",
+		tags : ["entry"],
+		place : "EMDEN",
+		lat : 53.360000,
+		lon : 7.210000
+	},
+	ALLOC_301080 : {
+		id : "ALLOC_301080",
+		name : "Emmen Gzi (NAM)",
+		tags : ["entry"],
+		place : "EMMEN",
+		lat : 52.500000,
+		lon : 6.230000
+	},
+	ALLOC_301082 : {
+		id : "ALLOC_301082",
+		name : "Flexicokersgas (ESSO)",
+		tags : ["entry"],
+		place : "ROTTERDAM",
+		lat : 51.869083,
+		lon : 4.293829
+	},
+	ALLOC_301083 : {
+		id : "ALLOC_301083",
+		name : "Gaag (NAM)",
+		tags : ["entry"],
+		place : "GAAG",
+		lat : 51.950000,
+		lon : 4.270000
+	},
+	ALLOC_301084 : {
+		id : "ALLOC_301084",
+		name : "Garijp (Total Fina)",
+		tags : ["entry"],
+		place : "GARIJP",
+		lat : 53.170000,
+		lon : 5.970000
+	},
+	ALLOC_301086 : {
+		id : "ALLOC_301086",
+		name : "Grijpskerk 1 (NAM)",
+		tags : ["entry"],
+		place : "GRIJPSKERK",
+		lat : 53.270000,
+		lon : 6.300000
+	},
+	ALLOC_301087 : {
+		id : "ALLOC_301087",
+		name : "Grijpskerk 2 (NAM)",
+		tags : ["entry"],
+		place : "GRIJPSKERK",
+		lat : 53.270000,
+		lon : 6.300000
+	},
+	ALLOC_301098 : {
+		id : "ALLOC_301098",
+		name : "Groningen (NAM)",
+		tags : ["entry"],
+		place : "GRONINGEN",
+		lat : 53.220000,
+		lon : 6.550000
+	},
+	ALLOC_301085 : {
+		id : "ALLOC_301085",
+		name : "Grootegast (NAM)",
+		tags : ["entry"],
+		place : "GROOTEGAST",
+		lat : 53.220000,
+		lon : 6.280000
+	},
+	ALLOC_301088 : {
+		id : "ALLOC_301088",
+		name : "Harlingen (Total Fina)",
+		tags : ["entry"],
+		place : "HARLINGEN",
+		lat : 53.180000,
+		lon : 5.420000
+	},
+	ALLOC_301452 : {
+		id : "ALLOC_301452",
+		name : "Ijmuiden (Wintershall)",
+		tags : ["entry"],
+		place : "IJMUIDEN",
+		lat : 52.460000,
+		lon : 4.610000
+	},
+	ALLOC_301068 : {
+		id : "ALLOC_301068",
+		name : "Koedijk (BP)",
+		tags : ["entry"],
+		place : "KOEDIJK",
+		lat : 52.670000,
+		lon : 4.750000
+	},
+	ALLOC_301089 : {
+		id : "ALLOC_301089",
+		name : "Kootstertille (NAM)",
+		tags : ["entry"],
+		place : "KOOTSTERTILLE",
+		lat : 53.220000,
+		lon : 6.100000
+	},
+	ALLOC_301090 : {
+		id : "ALLOC_301090",
+		name : "Maasvlakte (BP)",
+		tags : ["entry"],
+		place : "MAASVLAKTE",
+		lat : 51.950000,
+		lon : 4.050000
+	},
+	ALLOC_301091 : {
+		id : "ALLOC_301091",
+		name : "Marum (NAM)",
+		tags : ["entry"],
+		place : "MARUM",
+		lat : 53.150000,
+		lon : 6.270000
+	},
+	ALLOC_301311 : {
+		id : "ALLOC_301311",
+		name : "Middelie (NAM)",
+		tags : ["entry"],
+		place : "MIDDELIE",
+		lat : 52.530000,
+		lon : 5.020000
+	},
+	ALLOC_301092 : {
+		id : "ALLOC_301092",
+		name : "Middenmeer (Total Fina)",
+		tags : ["entry"],
+		place : "MIDDENMEER",
+		lat : 52.800000,
+		lon : 5.000000
+	},
+	ALLOC_301093 : {
+		id : "ALLOC_301093",
+		name : "Monster (NAM)",
+		tags : ["entry"],
+		place : "MONSTER",
+		lat : 52.030000,
+		lon : 4.180000
+	},
+	ALLOC_301095 : {
+		id : "ALLOC_301095",
+		name : "Opende Oost (NAM)",
+		tags : ["entry"],
+		place : "OPENDE OOST",
+		lat : 53.170000,
+		lon : 6.220000
+	},
+	ALLOC_301096 : {
+		id : "ALLOC_301096",
+		name : "Oude Pekela (NAM)",
+		tags : ["entry"],
+		place : "OUDE PEKELA",
+		lat : 53.100000,
+		lon : 7.020000
+	},
+	ALLOC_300145 : {
+		id : "ALLOC_300145",
+		name : "Oude Statenzijl (Ruhrgas-H)",
+		tags : ["entry"],
+		place : "OUDE STATENZIJL",
+		lat : 53.200000,
+		lon : 7.220000
+	},
+	ALLOC_301097 : {
+		id : "ALLOC_301097",
+		name : "Rotterdam Westgas (NAM)",
+		tags : ["entry"],
+		place : "ROTTERDAM",
+		lat : 51.920000,
+		lon : 4.500000
+	},
+	ALLOC_301101 : {
+		id : "ALLOC_301101",
+		name : "Ten Arlo (NAM)",
+		tags : ["entry"],
+		place : "TEN ARLOO",
+		lat : 52.697222,
+		lon : 6.434167
+	},
+	ALLOC_301094 : {
+		id : "ALLOC_301094",
+		name : "Uithuizen (Ngt)",
+		tags : ["entry"],
+		place : "UITHUIZEN",
+		lat : 53.420000,
+		lon : 6.670000
+	},
+	ALLOC_301106 : {
+		id : "ALLOC_301106",
+		name : "Ureterp (NAM)",
+		tags : ["entry"],
+		place : "URETERP",
+		lat : 53.100000,
+		lon : 6.170000
+	},
+	ALLOC_301107 : {
+		id : "ALLOC_301107",
+		name : "Vries 4 (NAM)",
+		tags : ["entry"],
+		place : "VRIES",
+		lat : 53.080000,
+		lon : 6.580000
+	},
+	ALLOC_301108 : {
+		id : "ALLOC_301108",
+		name : "Waalwijk (Clyde V144)",
+		tags : ["entry"],
+		place : "WAALWIJK",
+		lat : 51.680000,
+		lon : 5.070000
+	},
+	ALLOC_301109 : {
+		id : "ALLOC_301109",
+		name : "Warffum (NAM)",
+		tags : ["entry"],
+		place : "WARFFUM",
+		lat : 53.393333,
+		lon : 6.557778
+	},
+	ALLOC_301110 : {
+		id : "ALLOC_301110",
+		name : "Westerveld (NAM)",
+		tags : ["entry"],
+		place : "WESTERVELDE",
+		lat : 53.050000,
+		lon : 6.430000
+	},
+	ALLOC_301111 : {
+		id : "ALLOC_301111",
+		name : "Zelzate",
+		tags : ["entry"],
+		place : "ZELZATE",
+		lat : 51.200000,
+		lon : 3.810000
+	},
+	ALLOC_300131 : {
+		id : "ALLOC_300131",
+		name : "Hilvarenbeek Export",
+		tags : ["export"],
+		place : "HILVARENBEEK",
+		lat : 51.485291,
+		lon : 5.136000
+	},
+	ALLOC_300132 : {
+		id : "ALLOC_300132",
+		name : "Zevenaar Export",
+		tags : ["export"],
+		place : "ZEVENAAR",
+		lat : 51.930000,
+		lon : 6.070000
+	},
+	ALLOC_300133 : {
+		id : "ALLOC_300133",
+		name : "Winterswijk Export",
+		tags : ["export"],
+		place : "WINTERSWIJK",
+		lat : 51.970000,
+		lon : 6.720000
+	},
+	ALLOC_300134 : {
+		id : "ALLOC_300134",
+		name : "Zandvliet",
+		tags : ["export"],
+		place : "ZANDVLIET",
+		lat : 51.359692,
+		lon : 4.307660
+	},
+	ALLOC_300136 : {
+		id : "ALLOC_300136",
+		name : "Oude Statenzijl (EWE-G)",
+		tags : ["export"],
+		place : "OUDE STATENZIJL",
+		lat : 53.200000,
+		lon : 7.220000
+	},
+	ALLOC_300137 : {
+		id : "ALLOC_300137",
+		name : "Obbicht",
+		tags : ["export"],
+		place : "OBBICHT",
+		lat : 51.030000,
+		lon : 5.780000
+	},
+	ALLOC_300138 : {
+		id : "ALLOC_300138",
+		name : "Tegelen Export",
+		tags : ["export"],
+		place : "TEGELEN",
+		lat : 51.330000,
+		lon : 6.130000
+	},
+	ALLOC_300139 : {
+		id : "ALLOC_300139",
+		name : "Bocholtz",
+		tags : ["export"],
+		place : "BOCHOLTZ",
+		lat : 50.817471,
+		lon : 6.008610
+	},
+	ALLOC_300140 : {
+		id : "ALLOC_300140",
+		name : "Dinxperlo",
+		tags : ["export"],
+		place : "DINXPERLO",
+		lat : 51.870000,
+		lon : 6.480000
+	},
+	ALLOC_300141 : {
+		id : "ALLOC_300141",
+		name : "Haanrade",
+		tags : ["export"],
+		place : "HAANRADE",
+		lat : 50.880000,
+		lon : 6.080000
+	},
+	ALLOC_300142 : {
+		id : "ALLOC_300142",
+		name : "Vlieghuis",
+		tags : ["export"],
+		place : "VLIEGHUIS",
+		lat : 52.650000,
+		lon : 6.830000
+	},
+	ALLOC_300143 : {
+		id : "ALLOC_300143",
+		name : "'S-Gravenvoeren Export",
+		tags : ["export"],
+		place : "SGRAVENVOEREN",
+		lat : 50.759392,
+		lon : 5.758730
+	},
+	ALLOC_300144 : {
+		id : "ALLOC_300144",
+		name : "Oude Statenzijl (BEB-G)",
+		tags : ["export"],
+		place : "OUDE STATENZIJL",
+		lat : 53.200000,
+		lon : 7.220000
+	},
+	ALLOC_300146 : {
+		id : "ALLOC_300146",
+		name : "Oude Statenzijl (BEB-H)",
+		tags : ["export"],
+		place : "OUDE STATENZIJL",
+		lat : 53.200000,
+		lon : 7.220000
+	},
+	ALLOC_300147 : {
+		id : "ALLOC_300147",
+		name : "Oude Statenzijl (Wingas-H)",
+		tags : ["export"],
+		place : "OUDE STATENZIJL",
+		lat : 53.200000,
+		lon : 7.220000
+	},
+	ALLOC_301184 : {
+		id : "ALLOC_301184",
+		name : "Zandvliet (Fluxys-H)",
+		tags : ["export"],
+		place : "ZANDVLIET",
+		lat : 51.359692,
+		lon : 4.307660
+	},
+	ALLOC_301214 : {
+		id : "ALLOC_301214",
+		name : "Julianadorp Export",
+		tags : ["export"],
+		place : "JULIANADORP",
+		lat : 52.900000,
+		lon : 4.750000
+	}
+}
